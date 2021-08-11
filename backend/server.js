24cb264926779d344aba113554c1394776566f27
Smruthi Gowtham <smruthig01@gmail.com>
2021-08-12 00:10:12 +0530

    backend created and frontend statred

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose')
+const exerciseRouter = require('./routes/exercise')
+const userRouter = require('./routes/user')
 
 require('dotenv').config()
 
@@ -8,7 +10,16 @@ const app = express();
 const port = process.env.port || 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/NodeExpress', {useNewUrlParser: true, useUnifiedTopology:true,});
+const connection = mongoose.connection;
+connection.once('open',() => {
+    console.log('MongoDB connected')
+})
+
+app.use('/exercise', exerciseRouter);
+app.use('/user', userRouter);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
